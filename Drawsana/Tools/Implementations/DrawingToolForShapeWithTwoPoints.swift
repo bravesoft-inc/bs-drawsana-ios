e3863cdc3126c5bd7d5131ca34523a7191871d3d
朴根佑 <k.park@bravesoft.co.jp>
2022-03-18 13:08:15 +0900

    三角の選択エリアを正方形に補正

--- Drawsana/Tools/Implementations/DrawingToolForShapeWithTwoPoints.swift
+++ Drawsana/Tools/Implementations/DrawingToolForShapeWithTwoPoints.swift
@@ -46,6 +46,26 @@ open class DrawingToolForShapeWithTwoPoints: DrawingTool {
     guard var shape = shapeInProgress else { return }
     shape.b = point
       
+      if let ngonShape = shape as? NgonShape, ngonShape.sides == 3 {
+          //三角だけ正方形に修正
+          let diffrence = abs(shape.rect.width - shape.rect.height)
+          if shape.rect.width > shape.rect.height {
+              if shape.a.x > shape.b.x {
+                  shape.b.x += diffrence
+              } else {
+                  shape.b.x -= diffrence
+              }
+          } else {
+              if shape.a.y > shape.b.y {
+                  shape.b.y += diffrence
+              } else {
+                  shape.b.y -= diffrence
+              }
+          }
+      }
+      
+      
+      
       let translation = shape.rect.origin
       let boundingRect = shape.boundingRect
       
